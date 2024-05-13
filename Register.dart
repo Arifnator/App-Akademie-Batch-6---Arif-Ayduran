d1c45dc071927670b1b02c3199ebb598ec6a1578
Arifnator <arifayduran@icloud.com>
2024-05-13 10:55:13 +0200

    sfsdf

--- Register.dart
+++ Register.dart
@@ -1,26 +1,5 @@
-const String appName = "Purus Lern App";
+import "Variablen.dart";
 
-Map<String, List<String>> users = {
-  "arifnator@icloud.com".toLowerCase(): ["Arifnator".toLowerCase(), "Pass12."],
-  "ferro36@icloud.com".toLowerCase(): ["ferhatnator".toLowerCase(), "fero23."]
-};
-
-const List<String> activationCodes = [
-  "147084",
-  "273947",
-  "104739",
-  "173907",
-  "103738"
-];
-
-String inputLogin = "arifnaTor".toLowerCase();
-String inputPassword = "Pass12.";
-
-String regEmail = "ARvvvvR@icloud.com".toLowerCase();
-String regUserName = "Arifnatddor".toLowerCase();
-String regPassword1 = "Passsss1.";
-String regPassword2 = "Passsss1.";
-String activationCode = "104739";
 void main() {
   if (regEmail.isEmpty) {
     print("Bitte geben Sie eine Email ein!");
