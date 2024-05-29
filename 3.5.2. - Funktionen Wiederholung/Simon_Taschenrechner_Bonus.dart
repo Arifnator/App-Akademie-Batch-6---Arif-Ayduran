17c42133af77d332cad22c378e2f7368314c7527
Arifnator <arifayduran@icloud.com>
2024-05-29 18:55:22 +0300

    adasd

--- 3.5.2. - Funktionen Wiederholung/Simon_Taschenrechner_Bonus.dart
+++ 3.5.2. - Funktionen Wiederholung/Simon_Taschenrechner_Bonus.dart
@@ -22,31 +22,55 @@ möchte
 - “try catch” und wie man damit fehler behandelt
 */
 
-import "dart:io";
+import 'dart:io';
 
 void main() {
-  do {
-    print("Bitte geben Sie die erste Zahl an, mitdem Sie rechnen möchten");
-    double number1 = double.parse(stdin.readLineSync()!);
-    print("Bitte geben Sie die zweite Zahl an, mitdem Sie rechnen möchten");
-    double number2 = double.parse(stdin.readLineSync()!);
-    print(
-        "Bitte geben Sie die Methode en, mitdem Sie rechnen möchten. \n '1': Subtrahieren \n '2': Addieren \n '3': Multiplizieren \n '4': Dividieren");
-    int method = int.parse(stdin.readLineSync()!);
+  while (true) {
+    try {
+      print("Bitte geben Sie die erste Zahl an, mit der Sie rechnen möchten:");
+      double number1 = double.parse(stdin.readLineSync()!);
 
-    switch (method) {
-      case 1:
-        print("$number1 - $number2 = ${sub(number1, number2)}");
-      case 2:
-        print("$number1 + $number2 = ${add(number1, number2)}");
-      case 3:
-        print("$number1 * $number2 = ${multi(number1, number2)}");
-      case 4:
-        print("$number1 / $number2 = ${div(number1, number2)}");
-      default:
-        print("Keine gültige Eingabe");
+      print("Bitte geben Sie die zweite Zahl an, mit der Sie rechnen möchten:");
+      double number2 = double.parse(stdin.readLineSync()!);
+
+      print(
+          "Bitte geben Sie die Methode an, mit der Sie rechnen möchten. \n '1': Subtrahieren \n '2': Addieren \n '3': Multiplizieren \n '4': Dividieren");
+      int method = int.parse(stdin.readLineSync()!);
+
+      switch (method) {
+        case 1:
+          print("$number1 - $number2 = ${sub(number1, number2)}");
+          break;
+        case 2:
+          print("$number1 + $number2 = ${add(number1, number2)}");
+          break;
+        case 3:
+          print("$number1 * $number2 = ${multi(number1, number2)}");
+          break;
+        case 4:
+          print("$number1 / $number2 = ${div(number1, number2)}");
+          break;
+        default:
+          print("Keine gültige Eingabe");
+      }
+    } catch (e) {
+      print("Ein Fehler ist aufgetreten: ${e.toString()}");
     }
-  } while (true);
+
+    print("Möchten Sie eine weitere Berechnung durchführen? (j/n)");
+    String? continueCalc = stdin.readLineSync();
+    if (continueCalc != null) {
+      if (continueCalc.toLowerCase() == 'j') {
+        continue;
+      } else if (continueCalc.toLowerCase() == "n") {
+        break;
+      } else {
+        print("Bitte nur mit 'j' oder 'n' anwtorten");
+      }
+    } else {
+      print ("Keine Eingabe");
+    }
+  }
 }
 
 double sub(double a, double b) {
