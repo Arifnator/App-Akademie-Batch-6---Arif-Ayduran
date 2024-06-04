fdada38e2568fbb11164ab16269f98ece21d00f4
Arifnator <arifayduran@icloud.com>
2024-06-04 18:02:19 +0300

    mio

--- 3.6.2 | Klassen, Objekte, Konstruktoren/Land_und_Hauptstadt.dart
+++ 3.6.2 | Klassen, Objekte, Konstruktoren/Land_und_Hauptstadt.dart
@@ -1,35 +1,34 @@
 class Country {
   String name;
   int area;
-  int population;
+  double population;
   Capital capital;
 
   Country(this.name, this.area, this.population, this.capital);
 
   String returnCountryDetails() {
-    return "Das Land $name hat $area km2 Fläche und $population Einwohner. ${capital.returnCapitalDetails()}";
+    return "Das Land $name hat $area km2 Fläche und $population Mio. Einwohner. ${capital.returnCapitalDetails()}";
   }
 }
 
 class Capital {
   String name;
   int area;
-  int population;
+  double population;
   Capital(this.name, this.area, this.population);
 
   String returnCapitalDetails() {
-    return "Die Hauptstadt $name hat $area km2 Fläche und $population Einwohner.";
+    return "Die Hauptstadt $name hat $area km2 Fläche und $population Mio. Einwohner.";
   }
 }
 
 void main() {
   List<Country> countryList = [
-    Country("Deutschland", 357592, 83800000, Capital("Berlin", 891, 3645000)),
-    Country("Türkei", 783562, 84980000, Capital("Ankara", 24521, 5663000)),
+    Country("Deutschland", 357592, 83.80, Capital("Berlin", 891, 3.64)),
+    Country("Türkei", 783562, 84.98, Capital("Ankara", 24521, 5.66)),
   ];
 
-  countryList
-      .add(Country("Italien", 301340, 60360000, Capital("Rom", 1285, 2873000)));
+  countryList.add(Country("Italien", 301340, 60.36, Capital("Rom", 1285, 2.87)));
 
   countryList.forEach((country) => print(country.returnCountryDetails()));
 
