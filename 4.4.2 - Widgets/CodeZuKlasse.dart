e352445979a851410b92b3bfa8edba60e4cc25d8
Arifnator <arifayduran@icloud.com>
2024-06-25 15:52:56 +0300

    augae

---
+++ 4.4.2 - Widgets/CodeZuKlasse.dart
@@ -0,0 +1,76 @@
+// Betrachte folgenden Code aus einer Flutter App.
+// Deine Aufgabe ist es, diesen Code in reinem Dart nachzubauen.
+// Erstelle also eine Klasse MaterialApp, Scaffold, Center etc. mit entsprechenden Attributen.
+// Erzeuge dann eine Instanz dieser Klasse und weise sie einer Konstanten myApp zu.
+// Dein Dart Code soll am Ende dem Flutter Code aus dem Beispiel gleichen.
+
+// Hinweis: Es genügen die Attribute, die auch im Code-Beispiel zu sehen sind.
+
+// @override
+// Widget build(BuildContext context) {
+//   const myApp = MaterialApp(
+//     home: Scaffold(
+//       body: Center(
+//         child: Column(
+//           children: [
+//             Text("Hallo"),
+//           ],
+//         ),
+//       ),
+//     ),
+//   );
+
+//   return myApp;
+// }
+
+class BuildContext {}
+
+abstract class Widget {
+  final Widget? key;
+  const Widget({this.key});
+}
+
+class MaterialApp extends Widget {
+  final Widget? home;
+  const MaterialApp({this.home});
+}
+
+class Scaffold extends Widget {
+  final Widget? body;
+  const Scaffold({this.body});
+}
+
+class Center extends Widget {
+  final Widget? child;
+  const Center({this.child});
+}
+
+class Column extends Widget {
+  final List<Widget>? children;
+  const Column({this.children});
+}
+
+class Text extends Widget {
+  final String text;
+  const Text(this.text);
+}
+
+Widget build(BuildContext context) {
+  const Widget myApp = MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("Hallo"),
+          ],
+        ),
+      ),
+    ),
+  );
+  return myApp;
+}
+
+void main() {
+  BuildContext buildContext = BuildContext();
+  build(buildContext);
+}
